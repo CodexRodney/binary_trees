b106964e0260c62ca6a896a17a72379fbee8e546
CodexRodney <rodney002mwanje@gmail.com>
2022-02-17 14:26:41 -0800

    Removed the e;se clause and fixed line after it

--- 2-binary_tree_insert_right.c
+++ 2-binary_tree_insert_right.c
@@ -19,12 +19,10 @@ binary_tree_t *binary_tree_insert_right(binary_tree_t *parent, int value)
 		return (NULL);
 	if (parent->right != NULL)
 	{
-		new_node->right = parent->left;
+		new_node->right = parent->right;
 		parent->right->parent = new_node;
 	}
-	else
-	{
-		parent->right = new_node;
-	}
+	parent->right = new_node;
+
 	return (new_node);
 }
