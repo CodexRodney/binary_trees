9420d5c7d12b777660bf444cb8aa972af6f5a703
CodexRodney <rodney002mwanje@gmail.com>
2022-02-17 13:23:19 -0800

    Made corrections to files

--- 10-binary_tree_depth.c
+++ 10-binary_tree_depth.c
@@ -11,8 +11,8 @@ size_t binary_tree_depth(const binary_tree_t *tree)
 {
 	size_t depth;
 
-	if (tree == NULL)
+	if (tree == NULL && tree->parent)
 		return (0);
-	depth = binary_tree_depth(tree->parent) + 1;
+	depth = 1 + binary_tree_depth(tree->parent);
 	return (depth);
 }
